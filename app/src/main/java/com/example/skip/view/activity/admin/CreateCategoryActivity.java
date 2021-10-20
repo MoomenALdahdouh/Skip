915b6b2e1944f6054beed94953987bce03dc1085
mmsss <mmsss875@gmail.com>
2021-10-20 21:50:56 +0300

    ADMIN ADD CATEGORY

--- app/src/main/java/com/example/skip/view/activity/admin/CreateCategoryActivity.java
+++ app/src/main/java/com/example/skip/view/activity/admin/CreateCategoryActivity.java
@@ -1,16 +1,31 @@
 package com.example.skip.view.activity.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.skip.R;
+import com.example.skip.databinding.ActivityCreateAdminBinding;
+import com.example.skip.view.fragment.admin.category.CategoryDetailsFragment;
 
 public class CreateCategoryActivity extends AppCompatActivity {
 
+    private ActivityCreateAdminBinding binding;
+    private FragmentTransaction fragmentTransaction;
+    private Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_category);
+        binding = ActivityCreateAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        fragment = new CategoryDetailsFragment();
+        setFragment(fragment);
+    }
+
+    private void setFragment(Fragment fragment) {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment).addToBackStack(null).commit();
     }
 }
---
+++ app/src/main/java/com/example/skip/view/fragment/admin/category/CategoryImageFragment.java
@@ -0,0 +1,50 @@
+package com.example.skip.view.fragment.admin.category;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.skip.R;
+import com.example.skip.databinding.FragmentCategoryDetailsBinding;
+import com.example.skip.databinding.FragmentCategoryImageBinding;
+
+public class CategoryImageFragment extends Fragment {
+
+    private FragmentCategoryImageBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentCategoryImageBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        nextButton();
+    }
+
+    private void nextButton(){
+        binding.buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new CategoryBackgroundFragment();
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container, fragment).addToBackStack(null).commit();
+            }
+        });
+    }
+}
