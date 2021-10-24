9722c8e5278e916e6be2bc3737974ee7970abf2c
mmsss <mmsss875@gmail.com>
2021-10-24 21:46:44 +0300

    SELECT IMAGE FROM DEVICE AND COMPRESS

--- app/src/main/java/com/example/skip/view/fragment/admin/category/CategoryBackgroundFragment.java
+++ app/src/main/java/com/example/skip/view/fragment/admin/category/CategoryBackgroundFragment.java
@@ -1,9 +1,18 @@
 package com.example.skip.view.fragment.admin.category;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
@@ -12,6 +21,7 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.skip.R;
 import com.example.skip.databinding.FragmentCategoryBackgroundBinding;
@@ -19,11 +29,33 @@ import com.example.skip.databinding.FragmentCategoryImageBinding;
 import com.example.skip.model.Category;
 import com.example.skip.view.activity.admin.CreateCategoryActivity;
 import com.example.skip.viewmodel.CategoryViewModel;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.Random;
+
+import id.zelory.compressor.Compressor;
 
 public class CategoryBackgroundFragment extends Fragment {
 
     private FragmentCategoryBackgroundBinding binding;
     private Category myCategory;
+    private String imageName;
+    private Bitmap compressor;
+    private String downloadUri;
+    private Uri imageUri = null;
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    private static final int MAX_LENGTH = 100;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,23 +72,31 @@ public class CategoryBackgroundFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        selectImage();
         nextButton();
+    }
+
+    private void selectImage() {
+        binding.imageViewSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cropImage();
+            }
+        });
     }
 
     private void nextButton() {
         binding.buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveCategoryBackground();
-                /*Fragment fragment = new CategoryBackgroundFragment();
-                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.container, fragment).addToBackStack(null).commit();*/
+                //TODO: Show Dialog with green true image successfully create
+
             }
         });
     }
 
-    private void saveCategoryBackground() {
-        String background = "https://i.ibb.co/smbnYHG/2021-10-21-10-54-43.png";
+    private void saveCategoryBackground(String background) {
+        //String background = "https://i.ibb.co/smbnYHG/2021-10-21-10-54-43.png";
         ((CreateCategoryActivity) getActivity()).setCategoryBackground(background);
         myCategory = CategoryViewModel.getCategory();
         myCategory.setBackground(background);
@@ -72,6 +112,114 @@ public class CategoryBackgroundFragment extends Fragment {
                 categoryViewModel.setCategory(myCategory);
             }
         });*/
+    }
+
+    private void cropImage() {
+        imageUri = null;
+        imageName = "";
+        downloadUri = "";
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+        } else {
+            //TODO: Must add activity on Manifest file add this line code
+            //            AndroidManifest.xml
+            //            <activity
+            //            android:name="com.theartofdev.edmodo.cropper.CropImageActivity"
+            //            android:theme="@style/Base.Theme.AppCompat" />
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    //.setMinCropResultSize(512,512)
+                    .setAspectRatio(4, 2)
+                    .start(getContext(), CategoryBackgroundFragment.this);
+        }
+    }
+
+    //Crop image
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == getActivity().RESULT_OK) {
+                imageUri = result.getUri();
+                //imageViewItem.setImageURI(imageUri);
+                postBackgroundOnFireBase();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+    private void postBackgroundOnFireBase() {
+        if (imageUri != null) {
+            //progressBarBottomSheet.setVisibility(View.VISIBLE);
+            compressAndNameImage();
+            ByteArrayOutputStream byteArrayInputStream = new ByteArrayOutputStream();
+            compressor.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayInputStream);
+            byte[] thumpData = byteArrayInputStream.toByteArray();
+            StorageReference filePath = storageReference.child("category_background/").child(imageName);
+            UploadTask uploadTask = filePath.putBytes(thumpData);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+
+                            return filePath.getDownloadUrl();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                downloadUri = task.getResult().toString();
+                                saveCategoryBackground(downloadUri);
+                            }
+                        }
+
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                }
+            });
+        } else {
+            //Please upload image
+        }
+    }
+
+    //Name image
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(MAX_LENGTH);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++) {
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
+    private void compressAndNameImage() {
+        imageName = random() + ".jpg";
+        File imageFile = new File(imageUri.getPath());
+        try {
+            compressor = new Compressor(getContext())
+                    .setMaxHeight(240)
+                    .setMaxWidth(360)
+                    .setQuality(5)
+                    .compressToBitmap(imageFile);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
 }
--- app/src/main/java/com/example/skip/view/fragment/admin/category/CategoryImageFragment.java
+++ app/src/main/java/com/example/skip/view/fragment/admin/category/CategoryImageFragment.java
@@ -1,9 +1,17 @@
 package com.example.skip.view.fragment.admin.category;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
@@ -19,15 +27,36 @@ import com.example.skip.databinding.FragmentCategoryImageBinding;
 import com.example.skip.model.Category;
 import com.example.skip.view.activity.admin.CreateCategoryActivity;
 import com.example.skip.viewmodel.CategoryViewModel;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Random;
+
+import id.zelory.compressor.Compressor;
 
 public class CategoryImageFragment extends Fragment {
 
     private FragmentCategoryImageBinding binding;
     private Category myCategory;
+    private String imageName;
+    private Bitmap compressor;
+    private String downloadUri = null;
+    private Uri imageUri = null;
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    private static final int MAX_LENGTH = 100;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,14 +73,23 @@ public class CategoryImageFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        selectImage();
         nextButton();
+    }
+
+    private void selectImage() {
+        binding.imageViewSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cropImage();
+            }
+        });
     }
 
     private void nextButton() {
         binding.buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveCategoryImage();
                 Fragment fragment = new CategoryBackgroundFragment();
                 FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.replace(R.id.container, fragment).addToBackStack(null).commit();
@@ -59,12 +97,57 @@ public class CategoryImageFragment extends Fragment {
         });
     }
 
-    private void saveCategoryImage() {
-        String image = "https://i.ibb.co/dmvn6Dx/maintenance.png";
+    private void postImageOnFireBase() {
+        if (imageUri != null) {
+            //progressBarBottomSheet.setVisibility(View.VISIBLE);
+            compressAndNameImage();
+            ByteArrayOutputStream byteArrayInputStream = new ByteArrayOutputStream();
+            compressor.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayInputStream);
+            byte[] thumpData = byteArrayInputStream.toByteArray();
+            StorageReference filePath = storageReference.child("category_image/").child(imageName);
+            UploadTask uploadTask = filePath.putBytes(thumpData);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+
+                            return filePath.getDownloadUrl();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                downloadUri = task.getResult().toString();
+                                saveCategoryImage(downloadUri);
+                            }
+                        }
+
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                }
+            });
+        } else {
+            //Please upload image
+        }
+    }
+
+    private void saveCategoryImage(String image) {
+        //String image = "https://i.ibb.co/dmvn6Dx/maintenance.png";
         ((CreateCategoryActivity) getActivity()).setCategoryImage(image);
         myCategory = CategoryViewModel.getCategory();
         myCategory.setImage(image);
         CategoryViewModel.setCategory(myCategory);
+
+
         /*CategoryViewModel categoryViewModel = new ViewModelProvider(CategoryImageFragment.this).get(CategoryViewModel.class);
         categoryViewModel.getCategoryMutableLiveData().observe(getViewLifecycleOwner(), new Observer<Category>() {
             @Override
@@ -74,5 +157,68 @@ public class CategoryImageFragment extends Fragment {
                 categoryViewModel.setCategory(myCategory);
             }
         });*/
+    }
+
+    private void cropImage() {
+        imageUri = null;
+        imageName = "";
+        downloadUri = "";
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+        } else {
+            //TODO: Must add activity on Manifest file add this line code
+            //            AndroidManifest.xml
+            //            <activity
+            //            android:name="com.theartofdev.edmodo.cropper.CropImageActivity"
+            //            android:theme="@style/Base.Theme.AppCompat" />
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    //.setMinCropResultSize(512,512)
+                    .setAspectRatio(4, 4)
+                    .start(getContext(), CategoryImageFragment.this);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == getActivity().RESULT_OK) {
+                imageUri = result.getUri();
+                //imageViewItem.setImageURI(imageUri);
+                postImageOnFireBase();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+    //Name image
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(MAX_LENGTH);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++) {
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
+    private void compressAndNameImage() {
+        imageName = random() + ".jpg";
+        File imageFile = new File(imageUri.getPath());
+        try {
+            compressor = new Compressor(getContext())
+                    .setMaxHeight(240)
+                    .setMaxWidth(360)
+                    .setQuality(5)
+                    .compressToBitmap(imageFile);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
